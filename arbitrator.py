df161a6d32fc51e845fa9a2425197e2efb43c54a
herborgtveit <56346637+herborgtveit@users.noreply.github.com>
2019-10-18 13:23:25 +0200

    Add files via upload

---
+++ arbitrator.py
@@ -0,0 +1,24 @@
+import random
+class Arbitraitor:
+    """This class decides which behavior that wins"""
+
+    def choose_action(self, behaviors):
+        """Check all active behaviors and pick a winner"""
+        weight_range_array = []
+        winner = None
+        maximum_weight = 0.0
+        for behavior in behaviors:
+            old_max = maximum_weight
+            maximum_weight += behavior.weigth
+
+            array_range = [old_max, maximum_weight]
+            weight_range_array.append(array_range)
+
+        number = random.uniform(0.0, maximum_weight)
+
+        for array in weight_range_array:
+            if number >= array[0] and number <= array[1]:
+                print("The number is in behavior", weight_range_array.index(array))
+                winner = behaviors[weight_range_array.index(array)]
+
+        return winner.motor_recommendations, winner.halt_request
