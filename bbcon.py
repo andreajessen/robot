ca38cfd031d81fcba159ac96bf6e206e7e35c2b0
ronjaek <ekronja@gmail.com>
2019-10-18 10:55:17 +0200

    Opprettet en ny klasse Motors

--- bbcon.py
+++ bbcon.py
@@ -1,8 +1,21 @@
 """Higest-level class - require one instance"""
-
+from arbitrator import Arbitrator
 
 class BBCON:
     """class BBCON"""
 
     def __init__(self):
         behaviors = []
+        active_behaviors = []
+        sensobs = []
+        motobs = []
+        arbitrator = Arbitrator()
+
+        current_timestep = ""
+        inactive_behaviors = ""
+
+
+    def add_behavior(self):
+
+
+    def
