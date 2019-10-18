74ac273a539370eb4614283dce34cdcada678265
Ronjaek <55784114+Ronjaek@users.noreply.github.com>
2019-10-18 10:53:38 +0200

    Add files via upload

---
+++ motors.py
@@ -0,0 +1,10 @@
+"""Class Motors"""
+
+
+class Motors:
+
+    def set_value(self, v):
+        """sets speed and direction to the motor via v, a vector of size two"""
+        
+    def stop(self):
+        """turn of both wheel motors"""
