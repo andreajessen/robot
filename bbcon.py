7e08a88421c5162bbe6c4ce176336766d147438b
solveigMohr <solveig.mohr@gmail.com>
2019-10-18 15:25:19 +0200

    added a main

--- bbcon.py
+++ bbcon.py
@@ -20,7 +20,7 @@ class BBCON:
         self.behavior_values = {'motor_duration': 0.5, 'min_distance': 5.0,
                                 'goPri': 1,'backwards': [-1, -1], "forwards": [1, 1], 'whitePri': 2,
                                 'white_scale': 0.9, 'turn': [1, 0], 'cameraPri': 5, 'stop': [0, 0],
-                                "red_scale": 0.95}
+                                "red_scale": 0.95, 'collitionPri': 4}
 
     def add_behavior(self, behavior):
         """legg til ny behavior"""
