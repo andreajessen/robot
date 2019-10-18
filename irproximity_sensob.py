7d935c2eba7284dd603f2f8b2a8fbda1a9be7e6a
ronjaek <ekronja@gmail.com>
2019-10-18 14:08:05 +0200

    Merge branch 'master' of https://github.com/andreajessen/robot
    # Conflicts:
    #	bbcon.py
    #	motors.py

--- irproximity_sensob.py
+++ irproximity_sensob.py
@@ -1,13 +1,15 @@
 """A subclass of Sensob"""
 from sensob import Sensob
-from ultrasonic import Ultrasonic
 from irproximity_sensor import IRProximitySensor
 
-
-class Distance(Sensob):
+class IRProximitySensob(Sensob):
     """Calculate the distance to the nearest obstacle by looking at the time differences"""
 
-    def __init__(self, sensors):
+    def __init__(self):
         """Initialize"""
         super().__init__()
+        self.sensors = [IRProximitySensor()]
+
+    def update(self):
+        """Overwrite the update-method in the superclass Sensob"""
 
---
+++ ultrasonic_sensob.py
@@ -0,0 +1,22 @@
+"""A subclass of Sensob"""
+from sensob import Sensob
+from ultrasonic import Ultrasonic
+
+class UltrasonicSensob(Sensob):
+    """Calculate the distance to the nearest obstacle by looking at the time differences"""
+
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.sensors = [Ultrasonic()]
+
+
+    def update(self):
+        """Overwrite the update-method in the superclass Sensob"""
+        for sensor in self.sensors:
+            sensor.update()
+
+        self.value = self.sensors[0].get_value()
+        print("Ultrasonic", self.value)
+        return self.value
+
