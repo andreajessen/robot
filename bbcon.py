14e96531eb5d9b6bde20207125f1f9d69487bac4
solveigMohr <solveig.mohr@gmail.com>
2019-10-21 13:46:17 +0200

    changed whitecscale to 0.3 and redscale to 0.85

--- bbcon.py
+++ bbcon.py
@@ -21,8 +21,8 @@ class BBCON:
         self.arbitrator = Arbitrator()
         self.behavior_values = {'motor_duration': 0.5, 'min_distance': 5.0,
                                 'goPri': 1,'backwards': [-0.5, -0.5], "forward": [0.5, 0.5], 'whitePri': 2,
-                                'white_scale': 1, 'turn': [0.5, 0], 'cameraPri': 5, 'stop': [0, 0],
-                                "red_scale": 0.95, 'collitionPri': 4}
+                                'white_scale': 0.3, 'turn': [0.5, 0], 'cameraPri': 5, 'stop': [0, 0],
+                                "red_scale": 0.85, 'collitionPri': 4}
 
     def add_behavior(self, behavior):
         """legg til ny behavior"""
