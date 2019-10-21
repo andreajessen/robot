b1632cdebb69a3a3de99bdd742f0159e165b54a7
solveigMohr <solveig.mohr@gmail.com>
2019-10-21 13:07:29 +0200

    space

--- bbcon.py
+++ bbcon.py
@@ -20,8 +20,8 @@ class BBCON:
         self.motobs = []
         self.arbitrator = Arbitrator()
         self.behavior_values = {'motor_duration': 0.5, 'min_distance': 5.0,
-                                'goPri': 1,'backwards': [-0.5, -0.5], "forward": [0.5, 0.5], 'whitePri': 2,
-                                'white_scale': 0.9, 'turn': [0.5, 0], 'cameraPri': 5, 'stop': [0, 0],
+                                'goPri': 1,'backwards': [-1, -1], "forward": [1, 1], 'whitePri': 2,
+                                'white_scale': 0.9, 'turn': [1, 0], 'cameraPri': 5, 'stop': [0, 0],
                                 "red_scale": 0.95, 'collitionPri': 4}
 
     def add_behavior(self, behavior):
